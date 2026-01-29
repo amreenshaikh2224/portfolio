94edcc725efb0750bdb357aec386900f46b4bc6a
amreenshaikh2224 <amreenshaikh2224@gmail.com>
2026-01-29 12:13:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ export default App;
 //         </Routes>
 //       </HashRouter>
 //     </TooltipProvider>
-  </QueryClientProvider>
-);
+//   </QueryClientProvider>
+// );
 
-export default App;
+// export default App;
