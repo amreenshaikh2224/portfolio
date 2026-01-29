4e38c11ad8787427b51eca60e4f83707888e12a3
amreenshaikh2224 <amreenshaikh2224@gmail.com>
2026-01-29 12:23:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router-dom";
-
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -14,38 +12,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-
-      {/* ROUTES */}
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Index />
     </TooltipProvider>
   </QueryClientProvider>
 );
 
 export default App;
-
-
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import Index from "./pages/Index";
-
-// const queryClient = new QueryClient();
-
-// const App = () => (
-//   <QueryClientProvider client={queryClient}>
-//     <TooltipProvider>
-//       <Toaster />
-//       <Sonner />
-//       <Index />
-//     </TooltipProvider>
-//   </QueryClientProvider>
-// );
-
-// export default App;
 
 
 
